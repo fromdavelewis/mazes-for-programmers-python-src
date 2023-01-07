32587a70944cf0cd142673b77ad55728b6795539
Dave Lewis <dalewis@tikva.com>
2023-01-07 15:16:59 -0500

    3d support in recursive division.

--- algorithms/recursive_division.py
+++ algorithms/recursive_division.py
@@ -25,48 +25,44 @@ class RecursiveDivision(Algorithm):
             if cell.down is not None:
                 cell += cell.down
         self.divide(0, 0, 0, 0, grid.levels, grid.rows, grid.columns)
-        ASCIIExporter().render(self._grid)
+        # ASCIIExporter().render(self._grid)
 
     def divide(self, loop, level, row, column, depth, height, width):
         print("start", loop, level, row, column, depth, height, width)
         ASCIIExporter().render(self._grid)
         m = max(depth, height, width)
-        r = random.random() >= .1
-        print(r)
-        if height == 1 or width == 1 or (height <= 5 and width <=5 and r):
+        r = random.random() >= .9
+        if height == 1 or width == 1 or depth == 1 or (height <= 5 and width <=5 and depth <=5 and r):
             print("done", loop, level, row, column, depth, height, width)
             return
-        if m == width:
-            print("horiz", column, width)
-            divide_at = random.randint(column, column+width-2)
-            print("WDA", divide_at)
-            passage_at = random.randint(row, row+height-1)
-            print("WPA", passage_at)
+
+        if m == depth:
+            divide_at = random.randint(level, level+depth-2)
+            passage_at = [divide_at, random.randint(row, row+height-1), random.randint(column, column+width-1)]
             for x in range(row, row+height):
-                print("WC", depth-1, x, divide_at)
-                if x != passage_at:
-                    c = self._grid[depth-1, x, divide_at]
-                    c.unlink(c.east)
-            print("westside", loop+1, level, row, column, depth, height, divide_at+1-column)
+                for y in range(column, column+width):
+                    if [divide_at, x, y] != passage_at:
+                        c = self._grid[divide_at, x, y]
+                        c.unlink(c.up)
+            self.divide(loop+1, level, row, column, divide_at+1-level, height, width)
+            self.divide(loop+1, divide_at+1, row, column, depth+level-divide_at-1, height, width)
+        elif m == width:
+            divide_at = random.randint(column, column+width-2)
+            passage_at = [random.randint(level, level+depth-1), random.randint(row, row+height-1), divide_at]
+            for w in range(level, level+depth):
+                for x in range(row, row+height):
+                    if [w, x, divide_at] != passage_at:
+                        c = self._grid[w, x, divide_at]
+                        c.unlink(c.east)
             self.divide(loop+1, level, row, column, depth, height, divide_at+1-column)
-            print("eastside", loop+1, level, row, divide_at+1, depth, height, width+column-divide_at-1)
             self.divide(loop+1, level, row, divide_at+1, depth, height, width+column-divide_at-1)
         elif m == height:
-            print("vert", row, height)
             divide_at = random.randint(row, row+height-2)
-            print("HDA", divide_at)
-            passage_at = random.randint(column, column+width-1)
-            print("HPA", passage_at)
-            for x in range(column, column+width):
-                print("HC", depth-1, x, divide_at)
-                if x != passage_at:
-                    c = self._grid[depth-1, divide_at, x]
-                    c.unlink(c.south)
-            print("northside", loop+1, level, row, column, depth, divide_at+1-row, width)
+            passage_at = [random.randint(level, level+depth-1), divide_at, random.randint(column, column+width-1)]
+            for w in range(level, level+depth):
+                for y in range(column, column+width):
+                    if [w, divide_at, y] != passage_at:
+                        c = self._grid[w, divide_at, y]
+                        c.unlink(c.south)
             self.divide(loop+1, level, row, column, depth, divide_at+1-row, width)
-            print("southside", loop+1, level, divide_at+1, column, depth, height+row-divide_at-1, width)
             self.divide(loop+1, level, divide_at+1, column, depth, height+row-divide_at-1, width)
-        # if m == depth:
-        #     divide_at = random.randint(depth)
-        #     self.divide(level, row, column, divide_at, height, width)
-        #     self.divide(divide_at, row, column, depth-divide_at, height, width)
--- demos/terminal_demo.py
+++ demos/terminal_demo.py
@@ -18,7 +18,7 @@ AVAILABLE_ALGORITHMS = ALGORITHM_NAMES
 
 
 if __name__ == "__main__":
-    random.seed(1)
+    # random.seed(1)
     parser = argparse.ArgumentParser(description="Render a maze")
     parser.add_argument("levels", type=int, help="number of levels")
     parser.add_argument("rows", type=int, help="number of rows")
@@ -42,12 +42,14 @@ if __name__ == "__main__":
 
     grid = Grid3d(levels, rows, columns)
     algorithm.on(grid)
-    # exporter.render(grid)
-    # grid.braid(.5)
-    # exporter.render(grid)
+    exporter.render(grid)
+    grid.braid(.5)
+    exporter.render(grid)
 
     for num in range(rotations):
         grid = cast(DistanceGrid, Rotator().on(grid))
+
+    exporter.render(grid)
 
     if pathfinding:
         start, end = LongestPath.calculate(grid)
